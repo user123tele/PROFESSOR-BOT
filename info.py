7746e0cd7bf3297305d0598e698f01b03dc72a00
tamil4k <131458557+tamil4k@users.noreply.github.com>
2023-08-21 20:31:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,7 +28,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-PICS = (environ.get('PICS' ,'https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg https://graph.org/file/a125497b6b85a1d774394.jpg https://graph.org/file/43d26c54d37f4afb830f7.jpg https://graph.org/file/60c1adffc7cc2015f771c.jpg https://graph.org/file/d7b520240b00b7f083a24.jpg https://graph.org/file/0f336b0402db3f2a20037.jpg https://graph.org/file/39cc4e15cad4519d8e932.jpg https://graph.org/file/d59a1108b1ed1c6c6c144.jpg https://te.legra.ph/file/3a4a79f8d5955e64cbb8e.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg')).split()
+PICS = (environ.get('PICS' ,'https://telegra.ph/file/b0d0022fe71557b171406.jpg https://telegra.ph/file/bd36c4c27673c6a4147a0.jpg https://telegra.ph/file/f2e75a56c1827c41d7591.jpg https://telegra.ph/file/974ec5f53e7e5f6c7b401.jpg https://telegra.ph/file/71ec6502181e78dab48b9.jpg https://telegra.ph/file/dd579f573ff14657f498f.jpg https://telegra.ph/file/30ccee1f8b18ed2886dca.jpg https://telegra.ph/file/0418ea514ddd3dd141132.jpg https://telegra.ph/file/d45e8dbef6910f479f435.jpg')).split()
 BOT_START_TIME = time()
 
 # Admins, Channels & Users
